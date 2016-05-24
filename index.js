be5c0f3c179fc6fbb3b7f4534827861c35dd063d
anggito <anggitowibisono12@gmail.com>
2016-05-24 14:09:05 +0700

    node js getting started

---
+++ index.js
@@ -0,0 +1,38 @@
+/**
+ * Created by anggitowibisono2 on 5/23/2016.
+ */
+var onlineClient = [];
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.broadcast.emit('hi');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+        console.log(msg);
+    });
+
+    socket.on('registerClient',function(data){
+        var index = onlineClient.indexOf(data.user);
+        if (index > -1) {
+            onlineClient.splice(index, 1);
+        }
+       onlineClient.push(data.user);
+        socket.emit('onlineClient',onlineClient);
+        console.log(onlineClient);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
